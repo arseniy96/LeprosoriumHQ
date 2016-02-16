d889a29c7e7d9164ad858e4414bc8b942304bad1
arseniy96 <arsenzhar@gmail.com>
2016-02-16 12:25:02 +0300

    Copy repository Leprosorium

---
+++ app.rb
@@ -0,0 +1,45 @@
+#encoding: utf-8
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sqlite3'
+
+set :database, "sqlite3:leprosorium.db"
+
+class Post < ActiveRecord::Base
+
+end
+
+class Comment < ActiveRecord::Base
+
+end
+
+before do
+
+end
+
+configure do
+
+end
+
+get '/' do
+
+	erb :index
+end
+
+get '/new' do
+  erb :new
+end
+
+post '/new' do
+
+end
+
+get '/comments/:post_id' do
+
+  erb :comments
+end
+
+post '/comments/:post_id' do
+
+end
